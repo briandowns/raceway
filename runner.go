abe509299ec4f54323906f657e8a4db98899465f
Brian Downs <brian.downs@gmail.com>
2016-03-03 11:02:49 -0700

    fix errors in runner

--- runner.go
+++ runner.go
@@ -12,7 +12,7 @@ const CMD = "rally"
 var r = regexp.MustCompile("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")
 
 // Run exeutes the given task
-func TaskStart(scenario string, jsonOutput, HTMLOutput bool) error {
+func TaskStart(scenario string, jsonOutput, HTMLOutput bool, cmdStartArgs ...string) error {
 	cmdArgs := []string{"task", "start", scenario}
 	cmd := exec.Command(CMD, cmdStartArgs...)
 	cmdReader, err := cmd.StdoutPipe()
@@ -29,34 +29,38 @@ func TaskStart(scenario string, jsonOutput, HTMLOutput bool) error {
 			}
 		}
 	}()
-	if err = cmd.Start(); err {
+	if err = cmd.Start(); err != nil {
 		return err
 	}
-	if err = cmd.Wait(); err {
+	if err = cmd.Wait(); err != nil {
 		return err
 	}
 	if jsonOutput {
-		if err = generateJSON(taskID); err {
+		if err = generateJSON(taskID); err != nil {
 			return err
 		}
 	}
 	if HTMLOutput {
-		if err = generateHTML(taskID); err {
+		if err = generateHTML(taskID); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
+type Generator interface {
+	Generate(output string) error
+}
+
 // generateHTML builds HTML pages based
-func generateHTML(taskID string) error {
-	cmdArgs := []string{"task", "report", taskID, "--output", conf.HTMLOutputDir + "/" + taskID + ".html"}
+func generateHTML(taskID string, cmdStartArgs ...string) error {
+	cmdArgs := []string{"task", "report", taskID, "--output", Conf.HTMLOutputDir + "/" + taskID + ".html"}
 	cmd := exec.Command(CMD, cmdStartArgs...)
 	return nil
 }
 
 // generateJSON
-func generateJSON(taskID string) error {
+func generateJSON(taskID string, cmdStartArgs ...string) error {
 	cmdArgs := []string{"task", "results", taskID}
 	cmd := exec.Command(CMD, cmdStartArgs...)
 	return nil
