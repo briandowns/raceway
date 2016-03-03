286a94f4288fb50bbebddb7634ef4c25c239f7d5
Brian Downs <brian.downs@gmail.com>
2016-03-03 11:17:59 -0700

    redo the config package and update README

--- config/config.go
+++ config/config.go
@@ -5,34 +5,48 @@ import (
 	"os"
 )
 
+// Raceway holds the base settings for the app
+type Raceway struct {
+	AppPort  string `json:"app_port"`
+	AppDebug bool   `json:"app_debug"`
+}
+
+// Database
+type Database struct {
+	DBHost  string `json:"db_host"`
+	DBPort  int    `json:"db_port"`
+	DBUser  string `json:"db_user"`
+	DBPass  string `json:"db_pass"`
+	DBName  string `json:"db_name"`
+	DBDebug bool   `json:"db_debug"`
+}
+
+// Scenarios
+type Scenarios struct {
+	ScenarioDir    string `json:"scenario_dir"`
+	ScenarioFormat string `json:"scenario_format"`
+}
+
+// Graphite
+type Graphite struct {
+	GraphiteHost string `json:"graphite_host"`
+}
+
+// Scheduler
+type Scheduler struct {
+	SchedulerDBBucket  string `json:"scheduler_db_bucket"`
+	SchedulerDBName    string `json:"scheduler_db_name"`
+	SchedulerDBTimeout int    `json:"scheduler_db_timeout"`
+}
+
 // Config contains the Raceway configuration
-// TODO(briandowns) break this into individual struct types
 type Config struct {
-	Raceway struct {
-		AppPort  string `json:"app_port"`
-		AppDebug bool   `json:"app_debug"`
-	} `json:"raceway"`
-	Database struct {
-		DBHost  string `json:"db_host"`
-		DBPort  int    `json:"db_port"`
-		DBUser  string `json:"db_user"`
-		DBPass  string `json:"db_pass"`
-		DBName  string `json:"db_name"`
-		DBDebug bool   `json:"db_debug"`
-	} `json:"mysql"`
-	Scenarios struct {
-		ScenarioDir    string `json:"scenario_dir"`
-		ScenarioFormat string `json:"scenario_format"`
-	} `json:"scenarios"`
-	Graphite struct {
-		GraphiteHost string `json:"graphite_host"`
-	} `json:"graphite"`
-	Scheduler struct {
-		SchedulerDBBucket  string `json:"scheduler_db_bucket"`
-		SchedulerDBName    string `json:"scheduler_db_name"`
-		SchedulerDBTimeout int    `json:"scheduler_db_timeout"`
-	} `json:"scheduler"`
-	HTMLOutputDir string `json:"html_output_dir"`
+	Raceway       Raceway   `json:"raceway"`
+	Database      Database  `json:"database"`
+	Scenarios     Scenarios `json:"scenarios"`
+	Graphite      Graphite  `json:"graphite"`
+	Scheduler     Scheduler `json:"scheduler"`
+	HTMLOutputDir string    `json:"html_output_dir"`
 }
 
 // Load builds a config obj
